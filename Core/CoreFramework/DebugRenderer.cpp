4e2ffdd8142644da0310840838669a19080d8b3d
Daniel Everland <danieleverland@gmail.com>
2025-12-01 20:54:11 +0100

    Plot frametime

--- Core/CoreFramework/DebugRenderer.cpp
+++ Core/CoreFramework/DebugRenderer.cpp
@@ -5,6 +5,10 @@
 
 void DebugRenderer::Draw()
 {
+    constexpr int32_t HistogramSize = 100;
+    static std::array<float, HistogramSize> FPSHistory;
+    static int32_t FPSHistoryIdx = 0;
+    
     constexpr uint16_t HighMsThreshold = static_cast<uint16_t>(1.0f/60*1000);
     constexpr uint16_t MediumMsThreshold = static_cast<uint16_t>(1.0f/30*1000);
     
@@ -18,9 +22,18 @@ void DebugRenderer::Draw()
     ImGui::TextColored(GetRenderColor(HighMsThreshold, MediumMsThreshold, app.GameTimeMs, std::less()),     "GameTime: %dms", app.GameTimeMs);
     ImGui::TextColored(GetRenderColor(HighMsThreshold, MediumMsThreshold, app.RenderTimeMs, std::less()),   "RenderTime: %dms", app.RenderTimeMs);
 
+    // FPS
     uint16_t totalTime = app.GameTimeMs + app.RenderTimeMs;
     float fps = totalTime > 0 ? 1.0f / static_cast<float>(totalTime) * 1000.0f : 0.0f;
+    FPSHistory[FPSHistoryIdx] = fps;
+    FPSHistoryIdx++;
+    if (FPSHistoryIdx >= HistogramSize) FPSHistoryIdx = 0;
     ImGui::TextColored(GetRenderColor(60.0f, 30.0f, fps, std::greater_equal()),     "FPS: %.2f", fps);
+    
+    static float MaxFPS = fps;
+    if (fps > MaxFPS)
+        MaxFPS = fps;
+    ImGui::PlotLines("##DebugRendererFPSHistogram", FPSHistory.data(), HistogramSize, FPSHistoryIdx, NULL, 0, MaxFPS, ImVec2(100, 100));
     
     ImGui::End();
 }
