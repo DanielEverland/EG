f782f2815e88e6352c74062bd8c084c318aef3a5
Daniel Everland <danieleverland@gmail.com>
2025-12-06 19:12:30 +0100

    Minor update to fps graph rendering

--- Core/CoreFramework/DebugRenderer.cpp
+++ Core/CoreFramework/DebugRenderer.cpp
@@ -40,16 +40,13 @@ void DebugRenderer::DrawFrameTimeGraph()
 
     // FPS
     float totalTime = app.GameTimeMs + app.RenderTimeMs;
-    float fps = totalTime > 0 ? 1.0f / static_cast<float>(totalTime) * 1000.0f : 0.0f;
+    float fps = totalTime > 0 ? 1.0f / totalTime * 1000.0f : 0.0f;
     FPSHistory[FPSHistoryIdx] = fps;
-    FPSHistoryIdx++;
-    if (FPSHistoryIdx >= HistogramSize) FPSHistoryIdx = 0;
+    FPSHistoryIdx = ++FPSHistoryIdx % HistogramSize;
     ImGui::TextColored(GetRenderColor(60.0f, 30.0f, fps, std::greater_equal()),     "FPS: %.2f", fps);
     
-    static float MaxFPS = fps;
-    if (fps > MaxFPS)
-        MaxFPS = fps;
-    ImGui::PlotLines("##DebugRendererFPSHistogram", FPSHistory.data(), HistogramSize, FPSHistoryIdx, NULL, 0, MaxFPS, ImVec2(200, 100));
+    float maxElement = *std::ranges::max_element(FPSHistory);
+    ImGui::PlotLines("##DebugRendererFPSHistogram", FPSHistory.data(), HistogramSize, FPSHistoryIdx, NULL, 0, maxElement, ImVec2(200, 100));
     
     ImGui::End();
 }
